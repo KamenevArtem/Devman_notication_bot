d2de096bc3d03ce9e4c633b915c4d4ad49ebd997
KamenevArtem <112734642+KamenevArtem@users.noreply.github.com>
2023-04-04 02:05:13 +0300

    check completed tasks by Dev_API

---
+++ devman_API.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+from pprint import pprint
+
+def main():
+    load_dotenv()
+    access_token = os.environ['DEVMAN_API_TOKEN']
+    url = 'https://dvmn.org/api/user_reviews/'
+    headers = {
+        'Authorization': f'Token {access_token}'
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    pprint(response.json())
+
+
+if __name__ == '__main__':
+    main()
