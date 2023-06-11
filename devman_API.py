5ed9a4eec038ad4d655276664f517e162cb37ec3
KamenevArtem <kamenevart94@gmail.com>
2023-06-11 10:59:04 +0300

    Update devman_API.py

--- devman_API.py
+++ devman_API.py
@@ -8,11 +8,11 @@ from pprint import pprint
 def main():
     load_dotenv()
     access_token = os.environ['DEVMAN_API_TOKEN']
-    url = 'https://dvmn.org/api/user_reviews/'
+    reviews_url = 'https://dvmn.org/api/user_reviews/'
     headers = {
         'Authorization': f'Token {access_token}'
     }
-    response = requests.get(url, headers=headers)
+    response = requests.get(reviews_url, headers=headers)
     response.raise_for_status()
     pprint(response.json())
 
